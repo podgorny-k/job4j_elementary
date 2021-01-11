d247c0cf016cf80686d0ec10840af0472d3ee509
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-01-11 23:47:33 +0300

    3.1. Переназначение переменной [#446506]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -19,5 +19,9 @@ public class Calculator {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
+
+        int size = 10;
+        size = 100;
+        size = size - 5;
     }
 }
