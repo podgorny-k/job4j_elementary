c99bdeacfb8665bdbac5bf8124274149ccd3c72b
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-01-15 13:53:15 +0300

    5.7. Ипотека [#446557] v2

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
 
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        while (amount >= 0) {
+        while (amount > 0) {
             amount += amount * (percent / 100) - salary;
             year++;
         }
